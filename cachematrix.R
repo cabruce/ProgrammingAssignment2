15f468ca1f72ac585f3af5e2f6f5f7931ba8cde1
cabruce <courtney.a.bruce@gmail.com>
2021-03-08 12:43:08 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## This file creates two functions: 
+## This program creates two functions: 
         ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
         ## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already
         ## been calculated, and the matrix has not changed, then cacheSolve() will retrieve the inverse from the cache.
